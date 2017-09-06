2dd6a32975ecc0cbd240196f691b77d12040e61c
Dakota Lambert <dakotalambert117@gmail.com>
2017-09-06 09:07:13 -0400

    Reformatting / Button Color Changes
    Moved all hard coded HMTL into their respective typescript files for
    the home page and honeybee introduction pages.

--- src/pages/honeybee/honeybee.ts
+++ src/pages/honeybee/honeybee.ts
@@ -34,4 +34,16 @@ export class HoneybeePage {
 
   }
 
+  title: string = 'What is a Honeybee?';
+
+  image_name: string = "assets/images/honeybee_2.jpg";
+
+  page_content: string = `A honey bee (or honeybee) is any member of the genus Apis, primarily distinguished by the production and storage of honey
+  and the construction of perennial, colonial nests from wax. Currently, only seven species of honey bee are recognized,
+  with a total of 44 subspecies, though historically six to eleven species are recognized. The best known honey bee
+  is the Western honey bee which has been domesticated for honey production and crop pollination. Honey bees represent
+  only a small fraction of the roughly 20,000 known species of bees. Some other types of related bees produce and store
+  honey, including the stingless honey bees, but only members of the genus Apis are true honey bees. Feel free to go
+  through this app to learn more about the marvelous honeybee.`;
+
 }
