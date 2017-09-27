728c2ebd7828a86ac75d74b8af7f0ede644fe5dd
Dakota Lambert <dakotalambert117@gmail.com>
2017-09-27 12:29:09 -0400

    Removed unused code
    removed code that now lives in the JSON file

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -62,10 +62,6 @@ export class HomePage {
 
   title: string = "The Helpful Honeybee";
 
-  image_name: string = "assets/images/honeybee_1.png";
-  
-  page_content: string = `The purpose of this app is to educate about the wonders of the Honeybee and it's vital role in the natural world. Feel free
-  to follow the app in order or select a specific section from the menu to the left.`;
 
 
 
