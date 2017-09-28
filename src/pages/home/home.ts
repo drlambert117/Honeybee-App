156cadbe9f734343989f6f67970407d96ac55bb0
Dakota Lambert <dakotalambert117@gmail.com>
2017-09-28 15:34:48 -0400

    Refactoring

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,71 +1,62 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {DataService} from '../../services';
 
 import { HoneybeePage } from '../honeybee/honeybee';
-import {IPage} from '../../services/page';
 
-
+import { DataService } from '../../services';
+import { IPage } from '../../services/page';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
-  providers:[DataService]
-  
+  providers: [DataService]
 })
 export class HomePage {
-
+  title: string = "The Helpful Honeybee";
   pages: IPage[];
   page_data: IPage[];
   selectedPage: IPage[];
 
-  constructor(public navCtrl: NavController, public pageDataService : DataService) {
+  constructor(public navCtrl: NavController, public pageDataService: DataService) {
 
   }
 
-  ngOnInit(){
-    console.log("%c-----------------------","color: blue; font-weight: bold");
+  ngOnInit() {
+    console.log("%c-----------------------", "color: blue; font-weight: bold");
     console.log("%cHome Component", "color: blue; font-weight: bold");
-    console.log("%c-----------------------","color: blue; font-weight: bold");
+    console.log("%c-----------------------", "color: blue; font-weight: bold");
 
-    console.log("%c-----------------------","color: green; font-weight: bold");
+    console.log("%c-----------------------", "color: green; font-weight: bold");
     console.log("%cngOnInit() function called!", "color: green; font-weight: bold");
-    console.log("%c-----------------------","color: green; font-weight: bold");
+    console.log("%c-----------------------", "color: green; font-weight: bold");
 
-    
-    this.pageDataService.getData().subscribe(data => this.extractData(data));      
-            
 
-                }
+    this.pageDataService.getData().subscribe(data => this.extractData(data));
 
-   extractData(data: any){
-    console.log("%c-----------------------","color: green; font-weight: bold");
+
+  }
+
+  extractData(data: any) {
+    console.log("%c-----------------------", "color: green; font-weight: bold");
     console.log("%cextractData() function called!", "color: green; font-weight: bold");
-    console.log("%c-----------------------","color: green; font-weight: bold");
+    console.log("%c-----------------------", "color: green; font-weight: bold");
 
     this.pages = data;
     console.log("data:");
-    console.table(this.pages); 
+    console.table(this.pages);
 
     this.selectedPage = this.pages.filter(p => p.page_id == 1);
     console.log("selectedPage data: ");
     console.table(this.selectedPage);
-}
+  }
 
   goToHoneybeePage() {
-    console.log("%c-----------------------","color: green; font-weight: bold");
+    console.log("%c-----------------------", "color: green; font-weight: bold");
     console.log("%cgoToHoneybeePage() function was called.", "color: green; font-weight: bold");
-    console.log("%c-----------------------","color: green; font-weight: bold");
+    console.log("%c-----------------------", "color: green; font-weight: bold");
 
     this.navCtrl.setRoot(HoneybeePage);
 
   }
-
-  page_id: 1;
-
-  title: string = "The Helpful Honeybee";
-
-
-
 
 }
--- src/pages/honeybee/honeybee.ts
+++ src/pages/honeybee/honeybee.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {DataService} from '../../services';
 
 import { FastFactsPage } from '../fast-facts/fast-facts';
-import {IPage} from '../../services/page';
+
+import { DataService } from '../../services';
+import { IPage } from '../../services/page';
 
 @Component({
   selector: 'page-honeybee',
-  templateUrl: 'honeybee.html'
+  templateUrl: 'honeybee.html',
+  providers: [DataService]
 })
 export class HoneybeePage {
   title: string = 'What is a Honeybee?';
@@ -15,56 +17,56 @@ export class HoneybeePage {
   page_data: IPage[];
   selectedPage: IPage[];
 
-  constructor(public navCtrl: NavController, public pageDataService : DataService) {
+  constructor(public navCtrl: NavController, public pageDataService: DataService) {
 
   }
 
-  ngOnInit(){
-    console.log("%c-----------------------","color: blue; font-weight: bold");
+  ngOnInit() {
+    console.log("%c-----------------------", "color: blue; font-weight: bold");
     console.log("%cHoneyBee Component", "color: blue; font-weight: bold");
-    console.log("%c-----------------------","color: blue; font-weight: bold");
+    console.log("%c-----------------------", "color: blue; font-weight: bold");
 
-    console.log("%c-----------------------","color: green; font-weight: bold");
+    console.log("%c-----------------------", "color: green; font-weight: bold");
     console.log("%cngOnInit() function called!", "color: green; font-weight: bold");
-    console.log("%c-----------------------","color: green; font-weight: bold");
-    
-    this.pageDataService.getData().subscribe(data => this.extractData(data));      
-            
+    console.log("%c-----------------------", "color: green; font-weight: bold");
 
-                }
+    this.pageDataService.getData().subscribe(data => this.extractData(data));
 
-   extractData(data: any){
-    console.log("%c-----------------------","color: green; font-weight: bold");
+
+  }
+
+  extractData(data: any) {
+    console.log("%c-----------------------", "color: green; font-weight: bold");
     console.log("%cextractData() function called!", "color: green; font-weight: bold");
-    console.log("%c-----------------------","color: green; font-weight: bold");
+    console.log("%c-----------------------", "color: green; font-weight: bold");
 
     this.pages = data;
     console.log("data:");
-    console.table(this.pages); 
+    console.table(this.pages);
 
     this.selectedPage = this.pages.filter(p => p.page_id == 2);
     console.log("selectedPage data: ");
     console.table(this.selectedPage);
-}
+  }
 
   enlargePhotograph(id) {
 
-    if (document.getElementById(id).className == ''  ) {
-        document.getElementById(id).className ='enlargedPhotograph';
-        document.getElementById("enlargedImageBackground").className ='enlarged';
+    if (document.getElementById(id).className == '') {
+      document.getElementById(id).className = 'enlargedPhotograph';
+      document.getElementById("enlargedImageBackground").className = 'enlarged';
     }
 
     else {
-        document.getElementById(id).className ='';
-        document.getElementById("enlargedImageBackground").className ='';
+      document.getElementById(id).className = '';
+      document.getElementById("enlargedImageBackground").className = '';
     }
 
   }
 
   goToFastFactsPage() {
-    console.log("%c-----------------------","color: green; font-weight: bold");
+    console.log("%c-----------------------", "color: green; font-weight: bold");
     console.log("%cgoToFastFactsPage() function was called.", "color: green; font-weight:bold");
-    console.log("%c-----------------------","color: green; font-weight: bold");
+    console.log("%c-----------------------", "color: green; font-weight: bold");
 
     this.navCtrl.setRoot(FastFactsPage);
 
